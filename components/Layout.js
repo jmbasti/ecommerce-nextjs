0eefa1146eeb9dd215bcf7c123d24a2e0e7d8f18
jmbasti <markchristopherdauz@gmail.com>
2021-08-02 08:15:14 +0800

    add style to layout component

--- components/Layout.js
+++ components/Layout.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import Head from 'next/head';
 import { AppBar, Toolbar, Typography, Container } from '@material-ui/core';
+import useStyles from './../utils/styles';
 
 const Layout = ({ children }) => {
+  const classes = useStyles();
   return (
     <div>
       <Head>
         <title>Ecommerce</title>
       </Head>
-      <AppBar position="static">
+      <AppBar position="static" className={classes.navbar}>
         <Toolbar>
           <Typography>Ecommerce</Typography>
         </Toolbar>
       </AppBar>
-      <Container>{children}</Container>
-      <footer>
+      <Container className={classes.main}>{children}</Container>
+      <footer className={classes.footer}>
         <Typography>All rights reserved. Ecommerce</Typography>
       </footer>
     </div>
